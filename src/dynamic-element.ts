501a72d69b0132199e68791b84d15883ca3b6405
postpostscript <danielrw7@gmail.com>
2025-06-15 22:18:43 -0400

    Finish refactor + add initial examples

--- src/dynamic-element.ts
+++ src/dynamic-element.ts
@@ -1,121 +1,180 @@
+export type TemplateContextMap = WeakMap<HTMLTemplateElement, TemplateContext>;
+
 /** Cached template computations */
 export type TemplateContext = {
   /** Pre-transformed fragment from source template */
   fragment: DocumentFragment;
 
   /** Functions replacing original template scripts */
-  scripts: Function[];
+  methods: Set<Function>;
+
+  attrs: {
+    [name: string]: string[] | undefined;
+  };
 };
 
-export class DynamicElement extends HTMLElement {
+const array = Array.from;
+
+const getAttribute = (el: Element, name: string) => {
+  return el.getAttribute(name);
+};
+
+const withQuerySelectorAll = <TElement extends Element, TResult>(
+  query: string,
+  callback: (element: TElement) => TResult,
+  root: Pick<Element, "querySelectorAll"> = document
+): TResult[] => {
+  return array(root.querySelectorAll(query) as NodeListOf<TElement>, callback);
+};
+
+const cloneNode = <T extends Node>(node: T): T => {
+  return node.cloneNode(true) as T;
+};
+
+export default class DynamicElement extends HTMLElement {
   /** Global instance map used to get elements into scripts */
   static instance: { [id: string]: HTMLElement } = {};
 
-  /** Global template #templateContext map used to cache template computations */
-  static templates = new WeakMap<HTMLTemplateElement, TemplateContext>();
+  /** Global template templateContext map used to cache template computations */
+  static #templates = new WeakMap<HTMLTemplateElement, TemplateContext>();
 
   static install(tagName: string = "x-is") {
-    globalThis.DynamicElement = this;
-    customElements.define(tagName, this);
+    customElements.define(
+      tagName,
+      (globalThis.DynamicElement = DynamicElement)
+    );
+  }
+
+  static compile(template: HTMLTemplateElement): TemplateContext {
+    const cached = DynamicElement.#templates.get(template);
+    if (cached) {
+      return cached;
+    }
+
+    let result: TemplateContext = {
+      fragment: cloneNode<DocumentFragment>(template.content),
+      methods: new Set(),
+      attrs: Object.fromEntries(
+        array(template.attributes, (attr) => [attr.name, [attr.value]])
+      ),
+    };
+
+    withQuerySelectorAll(
+      "script:not([src])",
+      (script: HTMLScriptElement) => {
+        const type = getAttribute(script, "type");
+        if ([null, "text/javascript", "module"].includes(type)) {
+          let code = `{${script.innerHTML}}`;
+          if (type === "module") {
+            code = `return(async()=>${code})()`;
+          }
+          result!.methods.add(Function(code));
+          script.remove();
+        }
+      },
+      result.fragment
+    );
+
+    const extend = getAttribute(template, "extend");
+
+    if (extend) {
+      result = DynamicElement.combine([DynamicElement.load(extend), result]);
+    }
+
+    DynamicElement.#templates.set(template, result);
+
+    return result;
+  }
+
+  static combine(templateContexts: TemplateContext[]): TemplateContext {
+    const fragment: TemplateContext["fragment"] = new DocumentFragment();
+    const methods = new Set<Function>();
+    const attrs: TemplateContext["attrs"] = {};
+
+    templateContexts.forEach((context) => {
+      fragment.append(context.fragment);
+      for (const method of context.methods) {
+        methods.add(method);
+      }
+      for (const attrName in context.attrs) {
+        attrs[attrName] ??= [];
+        attrs[attrName].push(...context.attrs[attrName]!);
+      }
+    });
+
+    return {
+      fragment,
+      methods,
+      attrs,
+    };
+  }
+
+  static load(src: string): TemplateContext {
+    return DynamicElement.combine(
+      withQuerySelectorAll(src, DynamicElement.compile)
+    );
   }
 
   /** Instance ID used in `DynamicElement.instance` */
-  public id: string = crypto.randomUUID().slice(0, 8);
-  public internals: ElementInternals = this.attachInternals();
+  public id: string = crypto.randomUUID();
 
-  /** Shared copy of the source template */
-  public template: HTMLTemplateElement;
+  /** Shared copy of the source template context */
+  public compile(): TemplateContext {
+    return DynamicElement.load(getAttribute(this, "src")!);
+  }
+
+  public methods: Function[];
+
+  /** Create DocumentFragment to be appended to the shadowRoot */
+  public createFragment({
+    fragment: sourceFragment,
+    methods,
+  }: TemplateContext): DocumentFragment {
+    const fragment = cloneNode<DocumentFragment>(sourceFragment);
+
+    this.methods = array(methods);
+    // execute init script after all of the DOM loads
+    const newScript = document.createElement("script");
+    newScript.innerHTML = `DynamicElement.instance["${this.id}"].init()`;
+    fragment.append(newScript);
+
+    return fragment;
+  }
+
+  /** Execute the script Function equivalents with `this` accessible */
+  public init() {
+    const instance = this;
+    let fn;
+    while ((fn = instance.methods.shift())) {
+      const result = fn.call(instance);
+      if (result instanceof Promise) {
+        return result.then(instance.init.bind(instance));
+      }
+    }
+  }
 
   /** Alias for shadowRoot */
   #shadow = this.attachShadow({
     mode: "open",
   });
 
-  /** Cached template computations */
-  get #templateContext(): TemplateContext {
-    this.template ??= document.querySelector(
-      this.getAttribute("src")!
-    ) as HTMLTemplateElement;
-
-    let result = DynamicElement.templates.get(this.template);
-    if (!result) {
-      // remove "module" and "text/javascript" scripts and create Function equivalents
-      // will only happen once per template
-      result = {
-        fragment: this.template.content.cloneNode(true) as DocumentFragment,
-        scripts: [],
-      };
-
-      result.fragment
-        .querySelectorAll("script:not([src])")
-        .forEach((script) => {
-          const type = script.getAttribute("type");
-          if ([null, "text/javascript", "module"].includes(type)) {
-            let code = `{${script.innerHTML.trim()}}`;
-            if (type === "module") {
-              code = `(async () => ${code})()`;
-            }
-            result!.scripts.push(Function(code));
-            script.remove();
-          }
-        });
-
-      DynamicElement.templates.set(this.template, result);
-    }
-
-    return result;
-  }
-
-  /** Execute the script Function equivalents with `this` accessible */
-  init() {
-    this.#templateContext!.scripts.forEach((fn) => fn.call(this));
-  }
-
-  /** Create DocumentFragment to be appended to the shadowRoot */
-  #createFragment(): DocumentFragment {
-    const fragment = this.#templateContext.fragment.cloneNode(
-      true
-    ) as DocumentFragment;
-    if (this.#templateContext.scripts.length) {
-      const newScript = document.createElement("script");
-      newScript.innerHTML = `DynamicElement.instance["${this.id}"].init()`;
-      // script is executed after all of the DOM loads
-      fragment.appendChild(newScript);
-    }
-    return fragment;
-  }
+  #fragment: DocumentFragment;
 
   connectedCallback() {
-    DynamicElement.instance[this.id] = this;
-    this.#dispatch("connected");
-    this.#shadow.appendChild(this.#createFragment());
-  }
-
-  disconnectedCallback() {
-    delete DynamicElement.instance[this.id];
-    this.#dispatch("disconnected");
-  }
-
-  connectedMoveCallback() {
-    this.#dispatch("connectedMove");
-  }
-
-  adoptedCallback() {
-    this.#dispatch("adopted");
-  }
-
-  attributeChangedCallback(name, old, value) {
-    this.#dispatch("attributeChanged", {
-      detail: {
-        name,
-        value,
-        old,
-      },
+    const instance = this;
+    DynamicElement.instance[instance.id] = instance;
+    if (instance.#fragment) {
+      queueMicrotask(instance.init.bind(instance));
+    } else {
+      instance.#fragment = instance.createFragment(instance.compile());
+      instance.#shadow.append(instance.#fragment);
+    }
+    instance.methods.push(() => {
+      instance.dispatchEvent(new CustomEvent("connected"));
     });
   }
-
-  #dispatch<T>(type: string, eventInitDict?: CustomEventInit<T> | undefined) {
-    const event = new CustomEvent(type, eventInitDict);
-    this.dispatchEvent(event);
+  disconnectedCallback() {
+    delete DynamicElement.instance[this.id];
+    this.dispatchEvent(new CustomEvent("disconnected"));
   }
 }
